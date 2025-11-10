f03e366b595d43b46fd96e77787da31fb53432b4
aayushisood <soodaayushi27@gmail.com>
2025-11-10 23:33:32 +0530

    Create day44.c

---
+++ day44.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Q87: Count spaces, digits, and special characters in a string
+    char str[200];
+    int i, spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            spaces++;
+        else if (str[i] >= '0' && str[i] <= '9')
+            digits++;
+        else if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')))
+            special++;
+    }
+
+    printf("Spaces = %d\n", spaces);
+    printf("Digits = %d\n", digits);
+    printf("Special characters = %d\n", special);
+
+    // Q88: Replace spaces with hyphens in a string
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            str[i] = '-';
+    }
+
+    printf("Modified string: %s\n", str);
+
+    return 0;
+}
