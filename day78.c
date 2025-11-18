fe17ace82c0634ea199f84f208d87628aae4596f
aayushisood <soodaayushi27@gmail.com>
2025-11-18 22:43:50 +0530

    Create day78.c

---
+++ day78.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *file;
+    char ch;
+    int vowels = 0, consonants = 0;
+
+    file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("Error: Could not open input.txt\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF) {
+        ch = tolower(ch);
+
+        if (ch >= 'a' && ch <= 'z') {     // check only alphabets
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    fclose(file);
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
